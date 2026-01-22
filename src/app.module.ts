9c5d29da8747e8a5fd7f3ffeb38e817464fa321d
mc4rvalho <mcarvalho093@gmail.com>
2026-01-22 12:35:13 -0300

    Classe Usuario criada com todos os métodos e conectada no app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuario/usuario.module';
+import { Usuario } from './usuario/entities/usuario.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_meleva',
+      entities: [Usuario],
+      synchronize: true,
+    }),
+    UsuarioModule,
+  ],
+
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
