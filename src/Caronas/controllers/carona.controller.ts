000c9fba8b02c31ffee5bea00c1cf905f7035c38
Matheus Lins <matheusantonioslins@outlook.com>
2026-01-22 11:17:56 -0300

    Criação do CRUD da Classe Caronas

--- src/Caronas/controllers/carona.controller.ts
+++ src/Caronas/controllers/carona.controller.ts
@@ -1,4 +1,15 @@
-import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { Carona } from '../entities/carona.entity';
 import { CaronaService } from '../services/carona.service';
 
@@ -10,5 +21,35 @@ export class CaronaController {
   @HttpCode(HttpStatus.OK)
   findAll(): Promise<Carona[]> {
     return this.caronaService.findAll();
+  }
+
+  @Get('/:id')
+  @HttpCode(HttpStatus.OK)
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Carona> {
+    return this.caronaService.findById(id);
+  }
+
+  @Get('/destino/:destino')
+  @HttpCode(HttpStatus.OK)
+  findAllByDestino(@Param('destino') destino: string): Promise<Carona[]> {
+    return this.caronaService.findAllByDestino(destino);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() carona: Carona): Promise<Carona> {
+    return this.caronaService.create(carona);
+  }
+
+  @Put()
+  @HttpCode(HttpStatus.OK)
+  update(@Body() carona: Carona): Promise<Carona> {
+    return this.caronaService.update(carona);
+  }
+
+  @Delete('/:id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.caronaService.delete(id);
   }
 }
--- src/Caronas/services/carona.service.ts
+++ src/Caronas/services/carona.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
+import { DeleteResult } from 'typeorm/browser';
 import { Carona } from '../entities/carona.entity';
 
 @Injectable()
@@ -12,5 +13,41 @@ export class CaronaService {
 
   async findAll(): Promise<Carona[]> {
     return await this.caronaRepository.find();
+  }
+
+  async findById(id: number): Promise<Carona> {
+    const carona = await this.caronaRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!carona)
+      throw new HttpException('Carona não encontrada!', HttpStatus.NOT_FOUND);
+
+    return carona;
+  }
+  async findAllByDestino(destino: string): Promise<Carona[]> {
+    return await this.caronaRepository.find({
+      where: {
+        destino: ILike(`%${destino}%`),
+      },
+    });
+  }
+
+  async create(carona: Carona): Promise<Carona> {
+    return await this.caronaRepository.save(carona);
+  }
+
+  async update(carona: Carona): Promise<Carona> {
+    await this.findById(carona.id);
+
+    return await this.caronaRepository.save(carona);
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    await this.findById(id);
+
+    return await this.caronaRepository.delete(id);
   }
 }
