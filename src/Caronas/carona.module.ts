707513e76b455e8f369b9e5fbe070dc623be3e29
Matheus Lins <matheusantonioslins@outlook.com>
2026-01-22 10:38:29 -0300

    Registro das classes controller e service na classe carona module

--- src/Caronas/carona.module.ts
+++ src/Caronas/carona.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { Carona } from './entities/carona.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaronaService } from './services/carona.service';
+import { CaronaController } from './controllers/carona.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Carona])],
-  providers: [],
-  controllers: [],
+  providers: [CaronaService],
+  controllers: [CaronaController],
   exports: [],
 })
 export class CaronaModule {}
