8b73b140b00d9500a53aa3c657fb4a81e31be3f5
allysonaggp <allysonaggp@gmail.com>
2026-01-22 12:06:20 -0300

    correção reserva.module

--- src/Reservas/reserva.module.ts
+++ src/Reservas/reserva.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { Carona } from './entities/carona.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CaronaService } from './services/carona.service';
-import { CaronaController } from './controllers/carona.controller';
 import { Reserva } from './entities/reservas.entity';
 import { ReservaService } from './services/reservas.service';
 import { ReservaController } from './controllers/reservas.controller';
