5f8f0825b1f35d8ba2b8607e26e0a8eac30fb4d3
allysonaggp <allysonaggp@gmail.com>
2026-03-02 19:55:02 -0300

    teste local realizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { DevService } from './data/services/dev.service';
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useClass: ProdService,
+      useClass: DevService,
       imports: [ConfigModule],
     }),
     UsuarioModule,
